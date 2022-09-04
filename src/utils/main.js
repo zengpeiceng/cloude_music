a142fd7d5380f48d7e4e5e16e7b917d688a310c4
zengpeiceng <zengpeiceng@gmail.com>
2022-09-04 16:22:16 +0800

    update2

--- src/utils/main.js
+++ src/utils/main.js
@@ -7,8 +7,7 @@ export function request(config) {
       'Content-Type': 'application/json;charset=UTF-8',
     },
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://cloud-music-zengpeiceng.vercel.app/',
-    // baseURL:'https://netease-cloud-music-api-mu-liard.vercel.app/',
+    baseURL:'https://netease-cloud-music-api-1-git-master-zengpeiceng.vercel.app/',
     timeout: 5000,
   })
 
